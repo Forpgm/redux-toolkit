ad429ab63e00d7665c682dd5f2c58064612efe18
Forpgm <phm.giamy@gmail.com>
2024-09-04 21:44:13 +0700

    createAsyncThunk

--- remote-blog/src/components/PostList/PostList.tsx
+++ remote-blog/src/components/PostList/PostList.tsx
@@ -1,14 +1,23 @@
 import PostItem from 'components/PostItem'
+import SkeletonPost from 'components/Skeleton'
 
-import { deletePost, startEditingPost } from 'pages/blog/blog.slice'
-import { createRef } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { rootState } from 'store'
+import { deletePost, getPostList, startEditingPost } from 'pages/blog/blog.slice'
+
+import { createRef, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { rootState, useAppDispatch } from 'store'
 
 export const inputRef: React.RefObject<HTMLInputElement> | null = createRef<HTMLInputElement>()
 export default function PostList() {
+  const dispatch = useAppDispatch()
   const postList = useSelector((state: rootState) => state.blog.postList)
-  const dispatch = useDispatch()
+  const loading = useSelector((state: rootState) => state.blog.loading)
+  useEffect(() => {
+    const promise = dispatch(getPostList())
+    return () => {
+      promise.abort()
+    }
+  }, [dispatch])
 
   const handleDelete = (id: string) => {
     dispatch(deletePost(id))
@@ -28,11 +37,19 @@ export default function PostList() {
             Đừng bao giờ từ bỏ. Hôm nay khó khăn, ngày mai sẽ trở nên tồi tệ. Nhưng ngày mốt sẽ có nắng
           </p>
         </div>
-        <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
-          {postList.map((post) => (
-            <PostItem post={post} key={post.id} handleDelete={handleDelete} handleStartEditing={handleStartEditing} />
-          ))}
-        </div>
+        {loading && (
+          <>
+            <SkeletonPost />
+            <SkeletonPost />
+          </>
+        )}
+        {!loading && (
+          <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
+            {postList.map((post) => (
+              <PostItem post={post} key={post.id} handleDelete={handleDelete} handleStartEditing={handleStartEditing} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
