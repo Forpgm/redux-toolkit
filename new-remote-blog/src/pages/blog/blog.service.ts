f2f0ad4cbfd15102d03248ee43a2cc5d65178992
Forpgm <phm.giamy@gmail.com>
2024-09-07 23:13:45 +0700

    addPost using rtk query mutation

--- new-remote-blog/src/pages/blog/blog.service.ts
+++ new-remote-blog/src/pages/blog/blog.service.ts
@@ -1,14 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import Post from 'pages/types/blog.type'
-
+import Post from '../types/blog.type'
 export const blogApi = createApi({
   reducerPath: 'blogApi', // This is the name of the slice | tên field của Redux state
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/' }),
+  tagTypes: ['Posts'],
   endpoints: (builder) => ({
     // generic type theo thứ tự là kiểu response và argument
     getPosts: builder.query<Post[], void>({
-      query: () => 'posts' // method không có argument
+      query: () => 'posts', // method không có argument
+      providesTags(result) {
+        if (result) {
+          const final = [
+            ...result.map(({ id }) => ({ type: 'Posts' as const, id })),
+            { type: 'Posts' as const, id: 'LIST' }
+          ]
+          return final
+        }
+        const final = [{ type: 'Posts' as const, id: 'LIST' }]
+        return final
+      }
+    }),
+    addPost: builder.mutation<Post, Omit<Post, 'id'>>({
+      query: (body) => ({
+        url: 'posts',
+        method: 'POST',
+        body
+      }),
+      invalidatesTags: [{ type: 'Posts', id: 'LIST' }]
     })
   })
 })
-export const { useGetPostsQuery } = blogApi
+export const { useGetPostsQuery, useAddPostMutation } = blogApi
